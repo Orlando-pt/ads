c5b560568ae3ce609e7edbcf9e2528032e15cb46
Breno Salles <git@brenosalles.com>
2021-11-03 14:36:24 +0000

    feat: CI pipeline and upgraded JUnit
    Added CI pipeline that checks for unit tests and integration tests.
    
    Upgraded JUnit from version 4 to version 5.
    
    Removed irrelevant comments.

--- src/test/java/pt/up/fe/MainTest.java
+++ src/test/java/pt/up/fe/MainTest.java
@@ -1,12 +1,10 @@
 package pt.up.fe;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-/** Unit test for simple App. */
 public class MainTest {
-  /** Rigorous Test :-) */
   @Test
   public void shouldAnswerWithTrue() {
     assertTrue(true);
