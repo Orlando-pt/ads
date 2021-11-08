229b61f0e7223408fe0fb5a3c2a885a59d98ccbe
Orlando-pt <orlando.macedo15@ua.pt>
2021-11-08 20:02:30 +0000

    feat: implement Date

---
+++ src/main/java/pt/up/fe/dates/IBuilder.java
@@ -0,0 +1,12 @@
+package pt.up.fe.dates;
+
+public interface IBuilder {
+    IBuilder reset();
+    IBuilder switchDate();
+    IBuilder setYear(int year);
+    IBuilder setMonth(int month);
+    IBuilder setDay(int day);
+    IBuilder setHour(int hour);
+    IBuilder setMinute(int minute);
+    IBuilder setSecond(int second);
+}
