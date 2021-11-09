8c090fd4b6c75ebfad826c6fa67067ea8448abdf
diogofalken <diogofalken0@gmail.com>
2021-11-09 23:37:47 +0000

    feat: Added some of the functionality on Main
    - Now it is possible to add important fields on birth event like:
    	- Maternity
    	- Place of Birth
    	- Date of Birth
    	- Parents (Mother and Father)
    	- Description
    
    - There are some functions created as a mock since there are no implementations yet for them like `choosePlace()` and `createDate()`

--- src/main/java/pt/up/fe/Main.java
+++ src/main/java/pt/up/fe/Main.java
@@ -1,9 +1,14 @@
 package pt.up.fe;
 
+import pt.up.fe.dates.IBuilder;
 import pt.up.fe.dates.IDate;
+import pt.up.fe.dates.SimpleDate;
+import pt.up.fe.dates.SimpleDateBuilder;
 import pt.up.fe.events.*;
 import pt.up.fe.places.Place;
+import pt.up.fe.places.PlaceBuilder;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -17,26 +22,12 @@ public class Main {
 
             switch (selection) {
                 case 0:
-                    System.err.println ("Bye bye, have a nice day.\n");
+                    System.err.println("Bye bye, have a nice day.\n");
                     System.exit(0);
                     break;
                 case 1:
-                    // Menu logic
-                    List<String> eventTypes = Arrays.asList("Birth", "Death", "Emigration", "Marriage", "Residence", "Custom");
-                    System.out.println("What type of event do you want to create?");
-                    eventTypes.forEach(type -> {
-                        System.out.println(String.format("%s - %s", eventTypes.indexOf(type), type));
-                    });
-                    int chosenEvent = sc.nextInt();
-
-                    if(chosenEvent < 0 || chosenEvent > eventTypes.toArray().length - 1) {
-                        System.err.println("Invalid input.\n");
-                        System.exit(0);
-                    }
-
-                    Event newEvent = newEventInstance(eventTypes.get(chosenEvent), null, null);
-
-                    System.out.println(newEvent.getName());
+                    Event newEvent = createEvent(sc);
+                    System.out.println(newEvent.toString());
                     break;
                 default:
                     System.err.println("Invalid input.\n");
@@ -44,11 +35,31 @@ public class Main {
         }
     }
 
-    public static Event newEventInstance(String event, Place place, IDate date) {
+    public static Event createEvent(Scanner sc) {
+        // Menu logic
+        List<String> eventTypes = Arrays.asList("Birth", "Death", "Emigration", "Marriage", "Residence", "Custom");
+        System.out.println("What type of event do you want to create?");
+        eventTypes.forEach(type -> {
+            System.out.println(String.format("%s - %s", eventTypes.indexOf(type), type));
+        });
+        int chosenEvent = sc.nextInt();
+
+        if (chosenEvent < 0 || chosenEvent > eventTypes.toArray().length - 1) {
+            System.err.println("Invalid input.\n");
+            System.exit(0);
+        }
+
+        return newEventInstance(eventTypes.get(chosenEvent), sc);
+    }
+
+    public static Event newEventInstance(String event, Scanner sc) {
+        Place place = null;
+        IDate date = null;
+
         switch (event.toLowerCase()) {
             case "birth":
                 EventCreator birthCreator = new BirthCreator();
-                return  birthCreator.createEvent(place, date);
+                return populateBirthEvent(birthCreator.createEvent(place, date), sc);
             case "death":
                 EventCreator deathCreator = new DeathCreator();
                 return deathCreator.createEvent(place, date);
@@ -65,5 +76,91 @@ public class Main {
                 EventCreator customCreator = new CustomCreator(event);
                 return customCreator.createEvent(place, date);
         }
+    }
+
+    public static Event populateBirthEvent(Event birthEvent, Scanner sc) {
+        System.out.println("--- Birth Event ---");
+        System.out.println("\nMaternity:");
+        String maternity = sc.nextLine() + sc.nextLine();
+        birthEvent.addSpecialPurposeField("Maternity", maternity);
+
+        System.out.println("\nPlace of Birth:");
+        // TODO: List of possible places
+        System.out.println("1 - Lisboa");
+        int placeIndex = sc.nextInt();
+        birthEvent.addPlaceRelation("Place of Birth", choosePlace());
+
+        System.out.println("\nDate of Birth:");
+        IDate dateOfBirth = createDate(sc);
+        birthEvent.setDate(dateOfBirth);
+
+        // TODO: Function that list all the people and returns the chosen person
+        System.out.println("\nMother:");
+        Person mother = new Person();
+        System.out.println("Name of Mother: ");
+        mother.setName(sc.nextLine() + sc.nextLine());
+        birthEvent.addPeopleRelation("Mother", mother);
+        System.out.println("\nFather:");
+        Person father = new Person();
+        System.out.println("Name of Father: ");
+        father.setName(sc.nextLine());
+        birthEvent.addPeopleRelation("Father", father);
+
+        System.out.println("Description: ");
+        birthEvent.setDescription(sc.nextLine());
+
+        return birthEvent;
+    }
+
+    public static Place choosePlace() {
+        PlaceBuilder builder = new PlaceBuilder("Portugal");
+
+        return builder
+                .startCompound("Lisboa")
+                .startCompound("Lisboa")
+                .addParish("Benfica")
+                .addParish("Parque das Nações")
+                .endCompound()
+                .startCompound("Sintra")
+                .addParish("Mem Martins")
+                .addParish("Mercês")
+                .endCompound()
+                .endCompound()
+                .getResult();
+    }
+
+    public static IDate createDate(Scanner sc) {
+        int typeOfDate = 0;
+        while(typeOfDate != 1 && typeOfDate != 2) {
+            System.out.println("What kind of date you want to create?");
+            System.out.println("1 - Simple Date");
+            System.out.println("2 - Interval Date");
+            typeOfDate = sc.nextInt();
+        }
+
+        if(typeOfDate == 1) {
+            IBuilder dateBuilder = new SimpleDateBuilder().reset();
+
+            System.out.println("--- Simple Date constructor ---");
+            System.out.println("Year: ");
+            dateBuilder.setYear(sc.nextInt());
+            System.out.println("Month: ");
+            dateBuilder.setMonth(sc.nextInt());
+            System.out.println("Day: ");
+            dateBuilder.setDay(sc.nextInt());
+            System.out.println("Hour: ");
+            dateBuilder.setHour(sc.nextInt());
+            System.out.println("Minute: ");
+            dateBuilder.setMinute(sc.nextInt());
+            System.out.println("Second: ");
+            dateBuilder.setSecond(sc.nextInt());
+            System.out.println("... \n");
+
+            return dateBuilder.build();
+        }
+
+        // TODO: Implement interval date, create function with above logic and reuse twice
+
+        return null;
     }
 }
