24157874435a3d44a6b40493615e90bfcba59e02
Orlando-pt <orlando.macedo15@ua.pt>
2021-11-28 20:42:24 +0000

    remove unnecessary code

--- src/main/java/pt/up/fe/exports/DotExport.java
+++ src/main/java/pt/up/fe/exports/DotExport.java
@@ -2,14 +2,12 @@ package pt.up.fe.exports;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.tuple.ImmutablePair;
 
-import guru.nidi.graphviz.attribute.Color;
 import guru.nidi.graphviz.attribute.Font;
 import guru.nidi.graphviz.attribute.Rank;
 import guru.nidi.graphviz.attribute.Rank.RankDir;
@@ -17,11 +15,8 @@ import guru.nidi.graphviz.engine.Format;
 import guru.nidi.graphviz.engine.Graphviz;
 import guru.nidi.graphviz.model.Graph;
 import guru.nidi.graphviz.model.LinkSource;
-import guru.nidi.graphviz.model.LinkTarget;
-import guru.nidi.graphviz.model.MutableGraph;
 import static guru.nidi.graphviz.model.Factory.*;
 
-import pt.up.fe.person.Gender;
 import pt.up.fe.person.Person;
 
 public class DotExport {
@@ -30,32 +25,6 @@ public class DotExport {
 
     public DotExport(List<Person> personList) {
         this.personList = personList;
-    }
-
-    public void experimentGraphViz() throws Exception{
-        List<LinkTarget> children = Arrays.asList(mutNode("d"), mutNode("e"), mutNode("f"));
-
-        MutableGraph g = mutGraph("example1").setDirected(true).add(
-                mutNode("a").add(Color.RED).addLink(mutNode("b"), mutNode("c"))).add(
-                    mutNode("b").addLink(children)
-                );
-
-        Graphviz.fromGraph(g).width(200).render(Format.PNG).toFile(new File("example/ex1m.png"));
-    }
-
-    public void experimentNumber2() throws Exception {
-        Graph g = graph("example1").directed()
-        .graphAttr().with(Rank.dir(RankDir.LEFT_TO_RIGHT))
-        .nodeAttr().with(Font.name("arial"))
-        .linkAttr().with("class", "link-class")
-        .with(
-                node("a").with(Color.RED).link(node("b")),
-                node("a").link(node("d")),
-                node("a").link(node("z")),
-                node("b").link(node("c"))
-            );
-        Graphviz.fromGraph(g).height(100).render(Format.PNG).toFile(new File("example/ex1.png"));
-
     }
 
     public void createGraphHavingRoot(Person personRoot, String imageName) throws Exception {
@@ -72,7 +41,6 @@ public class DotExport {
     /**
      * create the tree
      */
-
     public List<LinkSource> getLinkSourcesTree(Person root) {
         List<LinkSource> linkSources = new ArrayList<>();
 
@@ -138,12 +106,4 @@ public class DotExport {
 
     }
 
-    /**
-     * export using dot language
-     */
-
-     /**
-      * export image
-      */
-    
 }
