8a3837888fde4335e7efce211a8247f48aa8d387
hugofpaiva <hugofpaiva@ua.pt>
2022-01-13 21:42:04 +0000

    feature: graphviz working

--- src/main/java/pt/up/fe/controllers/contentarea/graphviz/GraphVizPageController.java
+++ src/main/java/pt/up/fe/controllers/contentarea/graphviz/GraphVizPageController.java
@@ -7,10 +7,14 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 import pt.up.fe.controllers.contentarea.IContentPageController;
+import pt.up.fe.facades.PersonFacade;
 import pt.up.fe.helpers.CustomSceneHelper;
+import pt.up.fe.helpers.events.PageToSendCustomEvent;
 import pt.up.fe.helpers.events.PersonCustomEvent;
+import pt.up.fe.helpers.events.SelectModeCustomEvent;
 import pt.up.fe.person.Person;
 
 public class GraphVizPageController implements Initializable, IContentPageController {
@@ -39,7 +43,7 @@ public class GraphVizPageController implements Initializable, IContentPageContro
 
   @Override
   public void setEventHandlers() {
-    CustomSceneHelper.getNodeById("viewEditPersonPage").addEventFilter(
+    CustomSceneHelper.getNodeById("graphVizPage").addEventFilter(
         PersonCustomEvent.PERSON, new EventHandler<PersonCustomEvent>() {
           @Override
           public void handle(PersonCustomEvent personCustomEvent) {
@@ -48,6 +52,33 @@ public class GraphVizPageController implements Initializable, IContentPageContro
             selectPersonLabel.setVisible(true);
           }
         });
+  }
+
+  @FXML
+  private void selectPerson(MouseEvent event) {
+    CustomSceneHelper.getNodeById("listPersonsPage")
+        .fireEvent(new SelectModeCustomEvent(SelectModeCustomEvent.SELECT_MODE, true));
+    CustomSceneHelper.getNodeById("listPersonsPage")
+        .fireEvent(new PageToSendCustomEvent(PageToSendCustomEvent.PAGE_TO_SEND, "graphVizPage"));
+    CustomSceneHelper.bringNodeToFront("listPersons", "Page");
+  }
+
+
+  @FXML
+  private void generate(MouseEvent event) {
+    if (selectedPerson != null && !inputNamePNG.getText().isEmpty()) {
+      try {
+        PersonFacade.createFamilyTreeVisualization(selectedPerson, inputNamePNG.getText());
+        fileSavedLabel.setText("File has been saved to graphs/" + inputNamePNG.getText()
+            + ".png on the current directory");
+
+      } catch (Exception e) {
+        fileSavedLabel.setText("There was an error while exporting");
+        System.err.println("Error while exporting family tree.");
+        e.printStackTrace();
+      }
+      fileSavedLabel.setVisible(true);
+    }
 
   }
 
@@ -55,6 +86,10 @@ public class GraphVizPageController implements Initializable, IContentPageContro
   public void clearPage() {
     this.selectedPerson = null;
     selectPersonLabel.setVisible(false);
+    fileSavedLabel.setVisible(false);
+    inputNamePNG.clear();
+    fileSavedLabel.setVisible(false);
+
 
   }
 }
